a9c1b0c4dbbf6853a292003d14381003ac976783
piyu1234 <league@wts7.attlocal.net>
2018-06-06 18:26:01 -0700

    pi, stringmethods1

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 
 public class PiAloud {
 
+	public static void main(String[] args) {
+		
+	}
 	// 1. Make a main method and make sure your program can run
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
